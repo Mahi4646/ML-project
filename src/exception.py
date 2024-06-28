72d252b8d10bbeb317d88b688229a00e1cddcb2d
Mahi4646 <mahendranathjothindran@gamil.com>
2024-06-28 13:19:21 +0530

    loggin and exception

---
+++ src/exception.py
@@ -0,0 +1,33 @@
+import sys
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_details.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured ipython script name[{0}] line number[{1}]error message[{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+
+
+        return error_message
+
+ class CustomException(Exception):
+  def__init__(self,error_message,error_detail:sys):
+    super().__init__(error_message)
+    self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+
+    def__str__(self):
+        return self.error_message
+
+        
+if__name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomExpection(e,sys)
+
+    
+
+
+
